5576eb9946ec5a87b0c86944e5e79e6cece69856
Joshua Barton <joshuabarton510@gmail.com>
2026-05-06 16:36:14 -0600

    Pre-time complex splitting implementation

--- 1D/ComplexHeat.cpp
+++ 1D/ComplexHeat.cpp
@@ -14,6 +14,8 @@
 #include <mfem/fem/complex_fem.hpp>
 #include <mfem/fem/fe_coll.hpp>
 #include <mfem/fem/fespace.hpp>
+#include <mfem/fem/gridfunc.hpp>
+#include <mfem/fem/linearform.hpp>
 #include <mfem/general/array.hpp>
 #include <mfem/linalg/complex_operator.hpp>
 #include <mfem/linalg/ode.hpp>
@@ -25,15 +27,6 @@
 
 using namespace std;
 using namespace mfem;
-
-real_t u0_real_exact(const Vector &);
-real_t u0_imag_exact(const Vector &);
-
-void u1_real_exact(const Vector &, Vector &);
-void u1_imag_exact(const Vector &, Vector &);
-
-void u2_real_exact(const Vector &, Vector &);
-void u2_imag_exact(const Vector &, Vector &);
 
 bool check_for_inline_mesh(const char *mesh_file);
 
@@ -91,11 +84,14 @@ int main(int argc, char *argv[]) {
   const char *mesh_file = "2D_Grain.msh";
   int ref_levels = 1;
   int order = 2;
+
   real_t freq = -1.0;
   real_t heatFlux = 500;
 
+  real_t t_final = 1.0;
+  real_t dt = 1e-2;
+
   bool herm_conv = true;
-  bool exact_sol = true;
   bool pa = false;
 
   string source_name = "Heat Flux";
@@ -126,6 +122,7 @@ int main(int argc, char *argv[]) {
   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
                  "--no-partial-assembly", "Enable Partial Assembly.");
   args.Parse();
+
   if (!args.Good()) {
     args.PrintUsage(cout);
     return 1;
@@ -134,11 +131,6 @@ int main(int argc, char *argv[]) {
 
   if (freq > 0.0) {
     omega_ = 2.0 * M_PI * freq;
-  }
-
-  exact_sol = check_for_inline_mesh(mesh_file);
-  if (exact_sol) {
-    cout << "Identified a mesh with known exact solution" << endl;
   }
 
   ComplexOperator::Convention conv =
@@ -196,11 +188,10 @@ int main(int argc, char *argv[]) {
   // Define a finite element space on the mesh using Lagrange finite
   // elements
 
-  FiniteElementCollection *fec = NULL;
-  fec = new H1_FECollection(order, mesh->Dimension());
+  H1_FECollection fec(order, dim);
+  FiniteElementSpace fespace(mesh, &fec);
 
-  FiniteElementSpace *fespace = new FiniteElementSpace(mesh, fec);
-  cout << "Number of finite element unknowns: " << fespace->GetTrueVSize()
+  cout << "Number of finite element unknowns: " << fespace.GetTrueVSize()
        << endl;
 
   // 6. Determine the list of true (i.e. conforming) essential boundary dofs.
@@ -213,41 +204,34 @@ int main(int argc, char *argv[]) {
 
     ess_bdr.SetSize(mesh->bdr_attributes.Max());
     Array<int> ess_bdr_marker = bdr_attr_sets.GetAttributeSet(ess_name);
-    fespace->GetEssentialTrueDofs(ess_bdr_marker, ess_tdof_list);
+    fespace.GetEssentialTrueDofs(ess_bdr_marker, ess_tdof_list);
 
   } else if (mesh->bdr_attributes.Size()) {
     ess_bdr.SetSize(mesh->bdr_attributes.Max());
     ess_bdr = 0;
-    fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+    fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
   }
 
   Array<int> source_marker = attr_sets.GetAttributeSetMarker(source_name);
 
   // 7. Set up the linear form b(.) which corresponds to the right-hand side of
   //    the FEM linear system.
-  ComplexLinearForm b(fespace, conv);
+  ComplexLinearForm b(&fespace, conv);
+  LinearForm b_real(&fespace, &b.real());
+  LinearForm b_imag(&fespace, &b.imag());
   b = 0.0;
+  b_real = 0.0;
+  b_imag = 0.0;
 
   // 8. Define the solution vector u as a complex finite element grid function
   //    corresponding to fespace. Initialize u with initial guess of 1+0i or
   //    the exact solution if it is known.
-  ComplexGridFunction u(fespace);
-  ComplexGridFunction *u_exact = NULL;
-  if (exact_sol) {
-    u_exact = new ComplexGridFunction(fespace);
-  }
+  ComplexGridFunction u(&fespace);
 
   ConstantCoefficient thermalDiffusivity(alpha);
   ConstantCoefficient grainDiffusivity(alpha_grain);
   ConstantCoefficient thermalFlux(heatFlux);
   ProductCoefficient m_nbcCoef(alpha, thermalFlux);
-
-  FunctionCoefficient u0_r(u0_real_exact);
-  FunctionCoefficient u0_i(u0_imag_exact);
-  VectorFunctionCoefficient u1_r(dim, u1_real_exact);
-  VectorFunctionCoefficient u1_i(dim, u1_imag_exact);
-  VectorFunctionCoefficient u2_r(dim, u2_real_exact);
-  VectorFunctionCoefficient u2_i(dim, u2_imag_exact);
 
   ConstantCoefficient zeroCoef(0.0);
   ConstantCoefficient oneCoef(1.0);
@@ -256,37 +240,22 @@ int main(int argc, char *argv[]) {
   zeroVec = 0.0;
   Vector oneVec(dim);
   oneVec = alpha;
-  // oneVec[(prob == 2) ? (dim - 1) : 0] = 1.0;
+
   VectorConstantCoefficient zeroVecCoef(zeroVec);
   VectorConstantCoefficient oneVecCoef(oneVec);
 
-  if (exact_sol) {
-    u.ProjectBdrCoefficient(u0_r, u0_i, ess_bdr);
-    u_exact->ProjectCoefficient(u0_r, u0_i);
-  } else {
-    u.ProjectBdrCoefficient(oneCoef, zeroCoef, ess_bdr);
-  }
+  u.ProjectBdrCoefficient(oneCoef, zeroCoef, ess_bdr);
 
-  if (visualization && exact_sol) {
-    char vishost[] = "localhost";
-    int visport = 19916;
-    socketstream sol_sock_r(vishost, visport);
-    socketstream sol_sock_i(vishost, visport);
-    sol_sock_r.precision(8);
-    sol_sock_i.precision(8);
-    sol_sock_r << "solution\n"
-               << *mesh << u_exact->real() << "window_title 'Exact: Real Part'"
-               << flush;
-    sol_sock_i << "solution\n"
-               << *mesh << u_exact->imag()
-               << "window_title 'Exact: Imaginary Part'" << flush;
-  }
+  Vector u_real = u.real();
+  Vector u_complex = u.imag();
+  ConductionOperator oper_real(fespace, alpha, u_real);
+  ConductionOperator oper_imag(fespace, alpha, u_complex);
 
-  SesquilinearForm *a = new SesquilinearForm(fespace, conv);
+  SesquilinearForm *a = new SesquilinearForm(&fespace, conv);
   a->AddDomainIntegrator(new DiffusionIntegrator(thermalDiffusivity), NULL);
   a->AddDomainIntegrator(new DiffusionIntegrator(grainDiffusivity), NULL);
 
-  BilinearForm *pcOp = new BilinearForm(fespace);
+  BilinearForm *pcOp = new BilinearForm(&fespace);
 
   pcOp->AddDomainIntegrator(new DiffusionIntegrator(thermalDiffusivity));
   pcOp->AddDomainIntegrator(new DiffusionIntegrator(grainDiffusivity));
@@ -348,20 +317,6 @@ int main(int argc, char *argv[]) {
   //     errors if the exact solution is known.
   a->RecoverFEMSolution(U, b, u);
 
-  if (exact_sol) {
-
-    real_t err_r = -1.0;
-    real_t err_i = -1.0;
-
-    err_r = u.real().ComputeL2Error(u0_r);
-    err_i = u.imag().ComputeL2Error(u0_i);
-
-    cout << endl;
-    cout << "|| Re (u_h - u) ||_{L^2} = " << err_r << endl;
-    cout << "|| Im (u_h - u) ||_{L^2} = " << err_i << endl;
-    cout << endl;
-  }
-
   // 13. Save the refined mesh and the solution. This output can be viewed
   //     later using GLVis: "glvis -m mesh -g sol".
   {
@@ -395,24 +350,8 @@ int main(int argc, char *argv[]) {
                << *mesh << u.imag() << "window_title 'Solution: Imaginary Part'"
                << flush;
   }
-  if (visualization && exact_sol) {
-    *u_exact -= u;
-
-    char vishost[] = "localhost";
-    int visport = 19916;
-    socketstream sol_sock_r(vishost, visport);
-    socketstream sol_sock_i(vishost, visport);
-    sol_sock_r.precision(8);
-    sol_sock_i.precision(8);
-    sol_sock_r << "solution\n"
-               << *mesh << u_exact->real() << "window_title 'Error: Real Part'"
-               << flush;
-    sol_sock_i << "solution\n"
-               << *mesh << u_exact->imag()
-               << "window_title 'Error: Imaginary Part'" << flush;
-  }
   if (visualization) {
-    GridFunction u_t(fespace);
+    GridFunction u_t(&fespace);
     u_t = u.real();
     char vishost[] = "localhost";
     int visport = 19916;
@@ -441,10 +380,9 @@ int main(int argc, char *argv[]) {
 
   // 15. Free the used memory.
   delete a;
-  delete u_exact;
   delete pcOp;
-  delete fespace;
-  delete fec;
+  delete &fespace;
+  delete &fec;
   delete mesh;
 
   return 0;
@@ -521,47 +459,6 @@ bool check_for_inline_mesh(const char *mesh_file) {
   string s0 = file.substr((p0 == string::npos) ? 0 : (p0 + 1), 7);
   return s0 == "inline-";
 }
-
-complex<real_t> u0_exact(const Vector &x) {
-  int dim = x.Size();
-  complex<real_t> i(0.0, 1.0);
-  complex<real_t> alpha = i * omega_;
-  complex<real_t> kappa = std::sqrt(omega_);
-  return std::exp(-i * kappa * x[dim - 1]);
-}
-
-real_t u0_real_exact(const Vector &x) { return u0_exact(x).real(); }
-
-real_t u0_imag_exact(const Vector &x) { return u0_exact(x).imag(); }
-
-void u1_real_exact(const Vector &x, Vector &v) {
-  int dim = x.Size();
-  v.SetSize(dim);
-  v = 0.0;
-  v[0] = u0_real_exact(x);
-}
-
-void u1_imag_exact(const Vector &x, Vector &v) {
-  int dim = x.Size();
-  v.SetSize(dim);
-  v = 0.0;
-  v[0] = u0_imag_exact(x);
-}
-
-void u2_real_exact(const Vector &x, Vector &v) {
-  int dim = x.Size();
-  v.SetSize(dim);
-  v = 0.0;
-  v[dim - 1] = u0_real_exact(x);
-}
-
-void u2_imag_exact(const Vector &x, Vector &v) {
-  int dim = x.Size();
-  v.SetSize(dim);
-  v = 0.0;
-  v[dim - 1] = u0_imag_exact(x);
-}
-
 real_t InitialTemperature(const Vector &x) {
   if (x.Norml2() < 0.5) {
     return 2.0;
